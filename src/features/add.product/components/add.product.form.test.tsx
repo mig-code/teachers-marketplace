79b9349a83450260552b28dd3edc2e61cb394203
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-27 12:38:33 +0100

    :white_check_mark: Update AddForm test

--- src/features/add.product/components/add.product.form.test.tsx
+++ src/features/add.product/components/add.product.form.test.tsx
@@ -1,18 +1,67 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { BrowserRouter } from 'react-router-dom';
+import {
+    AppContext,
+    AppContextStructure,
+} from '../../../core/context/app.context';
 import { AddProductForm } from './add.product.form';
 
-describe('When render AddProductForm component', () => {
-    test('It should render the title', () => {
-        render(
-            <BrowserRouter>
-                <AddProductForm></AddProductForm>
-            </BrowserRouter>
-        );
-        const headingElement = screen.getByRole('heading', {
-            name: /Añadir Producto/i,
+describe('Given render AddProductForm component', () => {
+    const handleCreateProduct = jest.fn();
+    const inputMockData = {
+        title: 'Titulo del producto',
+        description: 'Descripcion del producto',
+        price: 10,
+    };
+
+    const mockAppContext = {
+        handleCreateProduct,
+    } as unknown as AppContextStructure;
+    describe('When it is rendered', () => {
+        render(<AddProductForm></AddProductForm>);
+        test('It should render the title', () => {
+            const headingElement = screen.getByRole('heading', {
+                name: /Añadir Producto/i,
+            });
+            expect(headingElement).toBeInTheDocument();
         });
-        expect(headingElement).toBeInTheDocument();
+        test('Then we should write in inputs and submit', () => {
+            render(
+                <AppContext.Provider value={mockAppContext}>
+                    <BrowserRouter>
+                        <AddProductForm></AddProductForm>
+                    </BrowserRouter>
+                </AppContext.Provider>
+            );
+
+            const titleInput = screen.getByLabelText(/Titulo del producto/i);
+            expect(titleInput).toBeInTheDocument();
+
+            const descriptionInput = screen.getByLabelText(
+                /Descripción del producto/i
+            );
+            expect(descriptionInput).toBeInTheDocument();
+
+            const priceInput = screen.getByLabelText(/Precio/i);
+            expect(priceInput).toBeInTheDocument();
+
+            const submitButton = screen.getByRole('button', {
+                name: /Añadir/i,
+            });
+            expect(submitButton).toBeInTheDocument();
+
+            userEvent.type(titleInput, inputMockData.title);
+            userEvent.type(descriptionInput, inputMockData.description);
+            userEvent.type(priceInput, inputMockData.price.toString());
+
+            expect(titleInput).toHaveValue(inputMockData.title);
+            expect(descriptionInput).toHaveValue(inputMockData.description);
+            expect(priceInput).toHaveValue(inputMockData.price);
+
+            userEvent.click(submitButton);
+            expect(handleCreateProduct).toHaveBeenCalled();
+        });
     });
 });
