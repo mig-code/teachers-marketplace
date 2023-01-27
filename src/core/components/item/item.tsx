ab2677338532e0a913ab7f4e8bb13fe85291f778
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-27 08:11:19 +0100

    :white_check_mark: Update item test

--- src/core/components/item/item.tsx
+++ src/core/components/item/item.tsx
@@ -5,13 +5,17 @@ import { ProductStructure } from '../../models/product';
 export default function Item({ item }: { item: ProductStructure }) {
     const { handleDeleteProduct, handleUpdateProduct } = useContext(AppContext);
 
-    function handleAddToFavorites() {
+    function handleClickAddToFavorites() {
         console.log('Añadir a favoritos');
         const newProduct = {
             ...item,
             isFavoritedBy: [...item.isFavoritedBy, 'user1'],
         };
         handleUpdateProduct(newProduct);
+    }
+    function handleClickDelete() {
+        console.log('Eliminar item');
+        handleDeleteProduct(item.localId);
     }
     return (
         <div>
@@ -21,10 +25,10 @@ export default function Item({ item }: { item: ProductStructure }) {
             <img src={item.imgUrl} alt={item.title} />
             <p>Precio : {item.price}</p>
             <p>Favorite by : {item.isFavoritedBy.map((user) => user + ' ')}</p>
-            <button onClick={() => handleDeleteProduct(item.localId)}>
-                Eliminar
+            <button onClick={handleClickDelete}>Eliminar</button>
+            <button onClick={handleClickAddToFavorites}>
+                Añadir a Favoritos
             </button>
-            <button onClick={handleAddToFavorites}>Añadir a Favoritos</button>
         </div>
     );
 }
