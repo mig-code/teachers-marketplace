e0cfd63824ccbe3618efd919e6c8ffae69b824a4
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-27 09:05:35 +0100

    :white_check_mark: Update ProductsRepository test

--- src/core/services/products.repository.test.tsx
+++ src/core/services/products.repository.test.tsx
@@ -1,4 +1,5 @@
 import { productMocks } from '../mocks/product.mocks';
+import { ProductStructure } from '../models/product';
 import { ProductsRepository } from './products.repository';
 
 describe('Given ProductsRepo', () => {
@@ -35,33 +36,68 @@ describe('Given ProductsRepo', () => {
             }).rejects.toThrowError();
             expect(global.fetch).toHaveBeenCalled();
         });
-        describe('When we use delete method', () => {
-            test(`Then if the ID are VALID, we received the localId 
+    });
+    describe('When we use delete method', () => {
+        test(`Then if the ID are VALID, we received the localId 
             of the task delete in the repo`, async () => {
-                const id = mockProducts[0].localId;
-                global.fetch = jest.fn().mockResolvedValue({
-                    ok: true,
-                    json: jest.fn().mockResolvedValue(id),
-                });
-                const data = await repo.delete(id);
-                expect(global.fetch).toHaveBeenCalled();
-                expect(data).toBe(id);
+            const id = mockProducts[0].localId;
+            global.fetch = jest.fn().mockResolvedValue({
+                ok: true,
+                json: jest.fn().mockResolvedValue(id),
             });
-            test(`Then if there are NOT ID, we received a null`, async () => {
-                await expect(async () => {
-                    await repo.delete('');
-                }).rejects.toThrowError();
-                expect(global.fetch).not.toHaveBeenCalled();
+            const data = await repo.delete(id);
+            expect(global.fetch).toHaveBeenCalled();
+            expect(data).toBe(id);
+        });
+        test(`Then if there are NOT ID, we received a null`, async () => {
+            await expect(async () => {
+                await repo.delete('');
+            }).rejects.toThrowError();
+            expect(global.fetch).not.toHaveBeenCalled();
+        });
+        test(`Then if the ID are NOT VALID, we received a null`, async () => {
+            global.fetch = jest.fn().mockResolvedValue({
+                ok: false,
             });
-            test(`Then if the ID are NOT VALID, we received a null`, async () => {
-                global.fetch = jest.fn().mockResolvedValue({
-                    ok: false,
-                });
-                await expect(async () => {
-                    await repo.delete('bad');
-                }).rejects.toThrowError();
-                expect(global.fetch).toHaveBeenCalled();
+            await expect(async () => {
+                await repo.delete('bad');
+            }).rejects.toThrowError();
+            expect(global.fetch).toHaveBeenCalled();
+        });
+    });
+    describe('When we use update method', () => {
+        test(`Then if the ID are VALID, we received the product 
+            update in the repo`, async () => {
+            const updatePayload: Partial<ProductStructure> = {
+                id: mockProducts[0].localId,
+                title: 'Product Updated',
+            };
+            global.fetch = jest.fn().mockResolvedValue({
+                ok: true,
+                json: jest.fn().mockResolvedValue(updatePayload),
             });
+            const data = await repo.update(updatePayload);
+            expect(global.fetch).toHaveBeenCalled();
+            expect(data).toEqual(updatePayload);
+        });
+        test(`Then if there are NOT ID, we received a null`, async () => {
+            await expect(async () => {
+                await repo.update({});
+            }).rejects.toThrowError();
+            expect(global.fetch).not.toHaveBeenCalled();
+        });
+        test(`Then if the ID are NOT VALID, we received a null`, async () => {
+            const updatePayload: Partial<ProductStructure> = {
+                id: 'bad',
+                title: 'Product Updated',
+            };
+            global.fetch = jest.fn().mockResolvedValue({
+                ok: false,
+            });
+            await expect(async () => {
+                await repo.update(updatePayload);
+            }).rejects.toThrowError();
+            expect(global.fetch).toHaveBeenCalled();
         });
     });
 });
