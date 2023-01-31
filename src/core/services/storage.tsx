dca3ae95c424aa88b7eee0ce36b462243e1f05e9
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-31 01:15:06 +0100

    :construction:  Add service storage remove hook

---
+++ src/core/services/storage.tsx
@@ -0,0 +1,19 @@
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../firebase/config';
+
+export function saveImageInStorage(
+    file: File,
+    storagePath: string,
+    fileName: string
+) {
+    const storageRef = ref(storage, storagePath + fileName);
+    const upload = uploadBytesResumable(storageRef, file);
+    return upload;
+}
+
+export function getUrlsFromStorage(storagePath: string, fileName: string) {
+    const storageRef = ref(storage, storagePath + fileName);
+    return getDownloadURL(storageRef);
+}
+
+
