a5ac191ed1f9a02645194bfb2ae410aeb13de13a
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-01 17:11:31 +0100

    :construction: add node env test firebaseconfig

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,15 +3,29 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINSENDERID,
-    appId: process.env.REACT_APP_APPID,
-    measurementId: process.env.REACT_APP_MEASUREMENTID,
-};
+let firebaseConfig = {};
+
+if (process.env.NODE_ENV === 'test') {
+    firebaseConfig = {
+        apiKey: 'test-api-key',
+        authDomain: 'test-auth-domain',
+        projectId: 'test-project-id',
+        storageBucket: 'test-storage-bucket',
+        messagingSenderId: 'test-messaging-sender-id',
+        appId: 'test-app-id',
+        measurementId: 'test-measurement-id',
+    };
+} else {
+    firebaseConfig = {
+        apiKey: process.env.REACT_APP_APIKEY,
+        authDomain: process.env.REACT_APP_AUTHDOMAIN,
+        projectId: process.env.REACT_APP_PROJECTID,
+        storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGINSENDERID,
+        appId: process.env.REACT_APP_APPID,
+        measurementId: process.env.REACT_APP_MEASUREMENTID,
+    };
+}
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
