1731e1f03a225c1c2ad247cbfcf5ec394db2b859
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-01 13:44:10 +0100

    :construction: render if user is logged :

--- src/features/home/pages/home.page.tsx
+++ src/features/home/pages/home.page.tsx
@@ -3,20 +3,29 @@ import Item from '../../../core/components/item/item';
 import { AppContext } from '../../../core/context/app.context';
 
 export function HomePage() {
-    const { handleLoadProducts, products } = useContext(AppContext);
+    const { handleLoadProducts, products, user, loginWithGoogle } =
+        useContext(AppContext);
 
     console.log('Loading home page with products: ', products);
+    console.log('Loading home page with user: ', user);
 
     return (
         <section>
             <h1>Dale una segunda vida al material escolar</h1>
-            <button onClick={handleLoadProducts}>Load products</button>
+            <p> Inicia sesión para poder cargar los productos</p>
+            {user?.info.firebaseId ? (
+                <button onClick={handleLoadProducts}>Load products</button>
+            ) : (
+                <button onClick={loginWithGoogle}>Login</button>
+            )}
+
             <div>
-                {products.map((item) => (
-                    <li key={item.firebaseId}>
-                        <Item item={item} />
-                    </li>
-                ))}
+                {user?.info.firebaseId &&
+                    products.map((item) => (
+                        <li key={item.firebaseId}>
+                            <Item item={item} />
+                        </li>
+                    ))}
             </div>
         </section>
     );
