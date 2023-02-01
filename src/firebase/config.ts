08f407b0e42f1c6c1320c8a30deab065975346c6
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-01 14:24:21 +0100

    :pencil2: change procces.nev variable name

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
     projectId: process.env.REACT_APP_PROJECTID,
     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    messagingSenderId: process.env.REACT_APP_MESSAGINSENDERID,
     appId: process.env.REACT_APP_APPID,
     measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
