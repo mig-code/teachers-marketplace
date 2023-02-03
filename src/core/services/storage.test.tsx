de76f9563a78ae7ea6326b7369ea919532530e9e
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-03 11:04:09 +0100

    :white_check_mark: Update test

--- src/core/services/storage.test.tsx
+++ src/core/services/storage.test.tsx
@@ -1,27 +1,80 @@
 import { saveImageInStorage, getUrlsFromStorage } from './storage';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import { storage } from '../../firebase/config';
+import * as debug from '../../tools/debug';
 
 jest.mock('firebase/storage');
+jest.mock('../../tools/debug');
 
-describe('Given "saveImageInStorage"', () => {
-    test('Then "saveImageInStorage" should be return a Upload task', async () => {
-        const file = new File([''], 'testImage.png', {
-            type: 'image/png',
+let spyConsole: jest.SpyInstance;
+
+const mockFile = new File([''], 'testimage2.png', {
+    type: 'image/png',
+});
+const mockStoragePath = 'images';
+const mockFileName = '2';
+
+describe('Given saveImageInStorage function', () => {
+    describe('Given "saveImageInStorage with valid ID"', () => {
+        beforeEach(() => {
+            (uploadBytesResumable as jest.Mock).mockResolvedValue(
+                mockStoragePath
+            );
+            ref as jest.Mock;
         });
-        const storageRef = ref(storage, 'images/1');
-        const uploadTask = await uploadBytesResumable(storageRef, file);
-        const result = await saveImageInStorage(file, 'test-url', 'images');
-        expect(result).toEqual(uploadTask);
-       
+
+        test('Then "saveImageInStorage" should return a Upload task', async () => {
+            const result = await saveImageInStorage(
+                mockFile,
+                mockStoragePath,
+                mockFileName
+            );
+            expect(ref).toHaveBeenCalled();
+            expect(uploadBytesResumable).toHaveBeenCalled();
+
+            await expect(result).toEqual(mockStoragePath);
+        });
+    });
+    describe('Given "saveImageInStorage with NOT  ID"', () => {
+        beforeEach(() => {
+            (uploadBytesResumable as jest.Mock).mockRejectedValue('error');
+            ref as jest.Mock;
+            spyConsole = jest.spyOn(debug, 'consoleDebug');
+        });
+        test('Then "saveImageInStorage" if its NOT VALID should be return a Upload task', async () => {
+            await saveImageInStorage(mockFile, mockStoragePath, mockFileName);
+
+            await expect(spyConsole).toHaveBeenCalled();
+        });
     });
 });
 
-describe('Given "getUrlsFromStorage"', () => {
-    test('Then "getUrlsFromStorage" should be return a url', async () => {
-        const storageRef = ref(storage, 'images/');
-        const downloadUrl = await getDownloadURL(storageRef);
-        const result = await getUrlsFromStorage('images', '2');
-        expect(result).toEqual(downloadUrl);
+describe('Given getUrlsFromStorage function', () => {
+    describe('Given valid ref', () => {
+        beforeEach(() => {
+            (getDownloadURL as jest.Mock).mockResolvedValue('url');
+            ref as jest.Mock;
+        });
+
+        test('Then "getUrlsFromStorage" should return a url', async () => {
+            const result = await getUrlsFromStorage(
+                mockStoragePath,
+                mockFileName
+            );
+            expect(ref).toHaveBeenCalled();
+            expect(getDownloadURL).toHaveBeenCalled();
+
+            await expect(result).toEqual('url');
+        });
+    });
+    describe('Given a not valid ref', () => {
+        beforeEach(() => {
+            (ref as jest.Mock).mockRejectedValue('error');
+            spyConsole = jest.spyOn(debug, 'consoleDebug');
+        });
+        test('Then "getUrlsFromStorage" should throw error when rejected', async () => {
+            await getUrlsFromStorage(mockStoragePath, mockFileName);
+
+            await expect(spyConsole).toHaveBeenCalled();
+        });
     });
 });
