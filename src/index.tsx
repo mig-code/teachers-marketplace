87d5b7347fbdb2524ece2d1a14aaf6da55daea4b
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-04 10:34:51 +0100

    :construction: Add redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './core/components/app/App';
 import './sass/index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-
 import { AppContextProvider } from './core/context/app.provider';
+import { store } from './core/store/store';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <AppContextProvider>
-            <Router>
-                <App></App>
-            </Router>
-        </AppContextProvider>
+        <Provider store={store}>
+            <AppContextProvider>
+                <Router>
+                    <App></App>
+                </Router>
+            </AppContextProvider>
+        </Provider>
     </React.StrictMode>
 );
 
