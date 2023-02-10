8b6b206cee58a038c2e47745350d2fd123b85384
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-10 11:12:23 +0100

    :children_crossing: Add scroll to top

--- src/core/components/layout/layout.tsx
+++ src/core/components/layout/layout.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
 import './layout.scss';
 
 export function Layout({ children }: { children: JSX.Element }) {
+    const pathname = useLocation();
+    useEffect(() => {
+        document.body.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }, [pathname]);
     return (
         <div className="layout-container">
             <Header></Header>
+
             <section className="layout-children">{children}</section>
 
             <Footer></Footer>
