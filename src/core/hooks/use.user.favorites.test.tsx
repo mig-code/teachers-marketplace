8106322904291a9e0b5da8af12145e25794d843c
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-11 11:46:43 +0100

    :construction: mount use.user.favorite.test

--- src/core/hooks/use.user.favorites.test.tsx
+++ src/core/hooks/use.user.favorites.test.tsx
@@ -1,26 +1,48 @@
+/* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/no-unnecessary-act */
+import { configureStore } from '@reduxjs/toolkit';
 import { render, screen, act, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { mockProduct1 } from '../../mocks/store.mock';
-import { store } from '../store/store';
+import { userReducer } from '../reducer/user.reducer';
+import { RootState, store } from '../store/store';
 import { ProductStructure } from '../types/products.types';
+import { UserStructure } from '../types/user.type';
+import { useProducts } from './use.products';
+
 import { useUserFavorites } from './use.user.favorites';
 
-jest.mock('./use.products');
-
 const mockHandleUpdateProduct = jest.fn();
-
-jest.mock('./use.products', () => ({
-    handleUpdateProduct: () => mockHandleUpdateProduct(),
-}));
+jest.mock('./use.products');
 
 describe(`Given useFavorites (custom hook)
             render with a virtual component`, () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
     let TestComponent: () => JSX.Element;
 
     let buttons: Array<HTMLElement>;
     const mockProduct: ProductStructure = mockProduct1;
+    const mockUser: UserStructure = {
+        info: {
+            firebaseId: 'validId',
+            name: '',
+            photoUrl: '',
+        },
+        token: '',
+    };
+    const preloadedState: Partial<RootState> = {
+        user: mockUser,
+    };
+
+    const mockStore = configureStore({
+        reducer: {
+            user: userReducer,
+        },
+        preloadedState,
+    });
 
     beforeEach(() => {
         TestComponent = () => {
@@ -32,40 +54,30 @@ describe(`Given useFavorites (custom hook)
                     <button onClick={handleAddToFavorites}>
                         Add to Favorites
                     </button>
-                    <button onClick={handleRemoveFromFavorites}>Logout</button>
+                    <button onClick={handleRemoveFromFavorites}>
+                        Remove from favorites
+                    </button>
                     <div>{mockProduct.isLikedBy?.users}</div>
                 </>
             );
         };
-
-        // eslint-disable-next-line testing-library/no-render-in-setup
-        render(
-            <Provider store={store}>
-                <TestComponent />
-            </Provider>
-        );
-        buttons = screen.getAllByRole('button');
     });
 
     describe('When we click on the button Add to Favorites', () => {
-        beforeEach(() => {
-            mockHandleUpdateProduct.mockResolvedValue({
-                ...mockProduct,
-                isLikedBy: {},
+        test.only('Then its function handleAddToFavorites should be called', async () => {
+            (useProducts as jest.Mock).mockReturnValue({
+                handleUpdateProduct: mockHandleUpdateProduct,
             });
-            mockHandleUpdateProduct.mockClear();
-        });
-        test('Then its function handleAddToFavorites should be called', async () => {
-            await act(async () => {
-                userEvent.click(buttons[0]);
-            });
-
-            expect(mockHandleUpdateProduct).toHaveBeenCalled();
-
+            render(
+                <Provider store={mockStore}>
+                    <TestComponent />
+                </Provider>
+            );
+            screen.debug();
+            buttons = screen.getAllByRole('button');
+            userEvent.click(buttons[0]);
             await waitFor(() => {
-                expect(store.getState().products[0].isLikedBy?.users).toBe(
-                    'validId'
-                );
+                expect(mockHandleUpdateProduct).toHaveBeenCalled();
             });
         });
     });
--- src/core/hooks/use.user.favorites.tsx
+++ src/core/hooks/use.user.favorites.tsx
@@ -6,7 +6,7 @@ import { useProducts } from './use.products';
 
 export function useUserFavorites(item: ProductStructure) {
     const user = useSelector((state: RootState) => state.user);
-  
+
     const { handleUpdateProduct } = useProducts();
 
     const handleAddToFavorites = useCallback(async () => {
@@ -59,64 +59,6 @@ export function useUserFavorites(item: ProductStructure) {
 
         await handleUpdateProduct(RemoveUserLike);
     }, [handleUpdateProduct, item, user.info.firebaseId]);
-
-    //     let AddUserLike: Partial<ProductStructure>;
-
-    //     if (!item.isLikedBy) {
-    //         AddUserLike = {
-    //             ...item,
-    //             isLikedBy: {
-    //                 users: [user.info.firebaseId],
-    //             },
-    //         };
-    //     } else {
-    //         AddUserLike = {
-    //             ...item,
-    //             isLikedBy: {
-    //                 ...item.isLikedBy,
-    //                 users: [...item.isLikedBy.users, user.info.firebaseId],
-    //             },
-    //         };
-    //     }
-    //     console.log('AddUserLike from Hook', AddUserLike);
-    //     handleUpdateProduct(AddUserLike);
-    // }
-    // function handleRemoveFromFavorites_() {
-    //     console.log('handleClickDeleteFromFavorites');
-    //     if (!item.isLikedBy) return;
-    //     const index = item.isLikedBy.users.indexOf(
-    //         user.info.firebaseId
-    //     ) as number;
-    //     console.log('index', index);
-
-    //     console.log('item.isLikedBy.users', item.isLikedBy.users);
-    //     let retunedArray: string[] = [];
-    //     if (item.isLikedBy.users.length === 1) {
-    //         retunedArray = [];
-    //     } else {
-    //         retunedArray = item.isLikedBy.users.filter(
-    //             (item, i) => i !== index
-    //         ) as string[];
-    //     }
-
-    //     console.log('retunedArray', retunedArray);
-    //     const deleteUserLike: Partial<ProductStructure> = {
-    //         ...item,
-    //         isLikedBy: {
-    //             ...item.isLikedBy,
-    //             users: retunedArray,
-    //         },
-    //     };
-    //     console.log('deleteUserLike', deleteUserLike);
-
-    //     handleUpdateProduct(deleteUserLike);
-    // }
-    // const getIfProductIsLikedByCurrentUser = () => {
-    //     if (item.isLikedBy) {
-    //         return item.isLikedBy.users.includes(user.info.firebaseId);
-    //     }
-    //     return false;
-    // };
 
     return {
         handleAddToFavorites,
