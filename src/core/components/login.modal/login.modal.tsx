6a78639d3916ccc5c226866e6ef8a9a52b87c9c1
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-15 13:11:21 +0100

    :construction: Add basic functionality

--- src/core/components/login.modal/login.modal.tsx
+++ src/core/components/login.modal/login.modal.tsx
@@ -1,19 +1,43 @@
+import { useState } from 'react';
+import { useUserAuth } from '../../hooks/use.user.auth';
 import './login.modal.scss';
 
 export function LoginModal() {
+    const [showModal, setShowModal] = useState(true);
+
+    const { handleLoginWithGoogle } = useUserAuth();
+    const handleLoginOnClick = async () => {
+        await handleLoginWithGoogle();
+        setShowModal(false);
+    };
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
     return (
-        <div className="login-modal">
-            <div className="login-modal__content">
-                <h2 className="login-modal__title">Bienvenido</h2>
-                <p className="login-modal__info">
-                    Para poder publicar tus artículos o añadirlos favoritos
-                    necesitas estar registrado
-                </p>
-                <button className="login-modal__close-button">x</button>
-                <button className="login-modal__google-button">
-                    <span>Login con Google</span>
-                </button>
-            </div>
-        </div>
+        <>
+            {showModal && (
+                <div className="login-modal">
+                    <div className="login-modal__content">
+                        <h2 className="login-modal__title">Bienvenido</h2>
+                        <p className="login-modal__info">
+                            Para poder publicar tus artículos o añadirlos
+                            favoritos necesitas estar registrado
+                        </p>
+                        <button
+                            onClick={handleCloseModal}
+                            className="login-modal__close-button"
+                        >
+                            x
+                        </button>
+                        <button
+                            onClick={handleLoginOnClick}
+                            className="login-modal__google-button"
+                        >
+                            <span>Login con Google</span>
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
