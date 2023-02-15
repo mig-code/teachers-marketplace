967c90ae2a66ff83e4cf34c43fc32dd2b8236068
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-15 16:34:54 +0100

    :white_check_mark: Update ButtonFav and Menu test

--- src/core/components/button.favorite/button.favorite.test.tsx
+++ src/core/components/button.favorite/button.favorite.test.tsx
@@ -10,52 +10,111 @@ import {
 import { userMockSameOwner } from '../../../mocks/user.mock';
 import userEvent from '@testing-library/user-event';
 import { ButtonFavorite } from './button.favorite';
+import {
+    modalReducer,
+    ModalState,
+} from '../../reducer/modal.reducer/modal.reducer';
+import { RootState } from '../../store/store';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { emptyMockUser } from '../../../mocks/store.mock';
 
-describe('When render Button Favorite component with valid user and product', () => {
+describe('Given Button Favorite component', () => {
     const mockHandleClickAddToFavorites = jest.fn();
     const mockHandleClickDeleteFromFavorites = jest.fn();
     const mockUser: UserStructure = userMockSameOwner;
+    const mockModalState: ModalState = {
+        loginModal: false,
+    };
+    const preloadedState: Partial<RootState> = {
+        modal: mockModalState,
+    };
 
-    describe('When product its not favorite', () => {
-        const mockProduct: ProductStructure = productMockWithEmptyIsLikedBy;
+    const mockStore = configureStore({
+        reducer: {
+            modal: modalReducer,
+        },
+        preloadedState,
+    });
+    describe('When render Button Favorite component with valid user and product', () => {
+        describe('When product its not favorite', () => {
+            const mockProduct: ProductStructure = productMockWithEmptyIsLikedBy;
 
-        test('It should be on the screen Add to Favorites', () => {
+            test('It should be on the screen Add to Favorites', () => {
+                render(
+                    <Provider store={mockStore}>
+                        <ButtonFavorite
+                            item={mockProduct}
+                            user={mockUser}
+                            handleClickAddToFavorites={
+                                mockHandleClickAddToFavorites
+                            }
+                            handleClickDeleteFromFavorites={
+                                mockHandleClickDeleteFromFavorites
+                            }
+                        ></ButtonFavorite>
+                    </Provider>
+                );
+                const buttonAdd = screen.getByText('Añadir a Favoritos');
+                expect(buttonAdd).toBeInTheDocument();
+
+                userEvent.click(buttonAdd);
+                expect(mockHandleClickAddToFavorites).toHaveBeenCalled();
+            });
+        });
+        describe('When product its already favorite', () => {
+            const mockProduct: ProductStructure =
+                productMockWithIsLikedBySameOwner;
+
+            test('It should be on the screen Delete from Favorites', () => {
+                render(
+                    <Provider store={mockStore}>
+                        <ButtonFavorite
+                            item={mockProduct}
+                            user={mockUser}
+                            handleClickAddToFavorites={
+                                mockHandleClickAddToFavorites
+                            }
+                            handleClickDeleteFromFavorites={
+                                mockHandleClickDeleteFromFavorites
+                            }
+                        ></ButtonFavorite>
+                    </Provider>
+                );
+                const buttonDelete = screen.getByText('Eliminar de Favoritos');
+                expect(buttonDelete).toBeInTheDocument();
+
+                userEvent.click(buttonDelete);
+                expect(mockHandleClickDeleteFromFavorites).toHaveBeenCalled();
+            });
+        });
+    });
+
+    describe('When render Button Favorite component with no user', () => {
+        test('It should be click openModal', () => {
+            const mockProduct: ProductStructure = productMockWithEmptyIsLikedBy;
+            const mockUser: UserStructure = emptyMockUser;
+
             render(
-                <ButtonFavorite
-                    item={mockProduct}
-                    user={mockUser}
-                    handleClickAddToFavorites={mockHandleClickAddToFavorites}
-                    handleClickDeleteFromFavorites={
-                        mockHandleClickDeleteFromFavorites
-                    }
-                ></ButtonFavorite>
+                <Provider store={mockStore}>
+                    <ButtonFavorite
+                        item={mockProduct}
+                        user={mockUser}
+                        handleClickAddToFavorites={
+                            mockHandleClickAddToFavorites
+                        }
+                        handleClickDeleteFromFavorites={
+                            mockHandleClickDeleteFromFavorites
+                        }
+                    ></ButtonFavorite>
+                </Provider>
             );
             const buttonAdd = screen.getByText('Añadir a Favoritos');
             expect(buttonAdd).toBeInTheDocument();
 
             userEvent.click(buttonAdd);
-            expect(mockHandleClickAddToFavorites).toHaveBeenCalled();
-        });
-    });
-    describe('When product its already favorite', () => {
-        const mockProduct: ProductStructure = productMockWithIsLikedBySameOwner;
-
-        test('It should be on the screen Delete from Favorites', () => {
-            render(
-                <ButtonFavorite
-                    item={mockProduct}
-                    user={mockUser}
-                    handleClickAddToFavorites={mockHandleClickAddToFavorites}
-                    handleClickDeleteFromFavorites={
-                        mockHandleClickDeleteFromFavorites
-                    }
-                ></ButtonFavorite>
-            );
-            const buttonDelete = screen.getByText('Eliminar de Favoritos');
-            expect(buttonDelete).toBeInTheDocument();
-
-            userEvent.click(buttonDelete);
-            expect(mockHandleClickDeleteFromFavorites).toHaveBeenCalled();
+            const modalStore = mockStore.getState().modal;
+            expect(modalStore.loginModal).toBe(true);
         });
     });
 });
--- src/core/components/menu/menu.test.tsx
+++ src/core/components/menu/menu.test.tsx
@@ -6,12 +6,14 @@ import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { RootState } from '../../store/store';
 import { configureStore } from '@reduxjs/toolkit';
-import { uploadImageReducer } from '../../reducer/upload.image.reducer/upload.image.reducer';
+
 import { userReducer } from '../../reducer/user.reducer/user.reducer';
 import { UserStructure } from '../../types/user.type';
-import { useUserAuth } from '../../hooks/use.user.auth';
 
-jest.mock('../../hooks/use.user.auth');
+import {
+    modalReducer,
+    ModalState,
+} from '../../reducer/modal.reducer/modal.reducer';
 
 describe('Given Menu component', () => {
     beforeEach(() => {
@@ -26,25 +28,23 @@ describe('Given Menu component', () => {
             },
             token: '',
         };
+        const mockModalState: ModalState = {
+            loginModal: false,
+        };
         const preloadedState: Partial<RootState> = {
             user: mockUser,
+            modal: mockModalState,
         };
 
         const mockStore = configureStore({
             reducer: {
-                uploadImage: uploadImageReducer,
                 user: userReducer,
+                modal: modalReducer,
             },
             preloadedState,
         });
 
         test('Then we should click in login button', () => {
-            const mockhandleLoginWithGoogle = jest.fn();
-            (useUserAuth as jest.Mock).mockReturnValue({
-                handleLoginWithGoogle: mockhandleLoginWithGoogle,
-                handleLogout: jest.fn(),
-            });
-
             render(
                 <Provider store={mockStore}>
                     <MemoryRouter>
@@ -55,30 +55,8 @@ describe('Given Menu component', () => {
             const buttonElement = screen.getAllByText('Login');
             userEvent.click(buttonElement[0]);
 
-            expect(mockhandleLoginWithGoogle).toHaveBeenCalled();
-        });
-        test('Then we should  check is active class', () => {
-            const mockhandleLogout = jest.fn();
-            (useUserAuth as jest.Mock).mockReturnValue({
-                handleLoginWithGoogle: jest.fn(),
-                handleLogout: mockhandleLogout,
-            });
-
-            render(
-                <Provider store={mockStore}>
-                    <MemoryRouter>
-                        <Menu />
-                    </MemoryRouter>
-                </Provider>
-            );
-            const buttonElement = screen.getAllByText('Publicar');
-            expect(buttonElement[0]).toHaveAttribute('class', 'nav-button');
-            userEvent.click(buttonElement[0]);
-
-            expect(buttonElement[0]).toHaveAttribute(
-                'class',
-                'nav-button nav-button--active'
-            );
+            const modalStore = mockStore.getState().modal;
+            expect(modalStore.loginModal).toBe(true);
         });
     });
     describe('When it has been render with user', () => {
@@ -96,19 +74,12 @@ describe('Given Menu component', () => {
 
         const mockStore = configureStore({
             reducer: {
-                uploadImage: uploadImageReducer,
                 user: userReducer,
             },
             preloadedState,
         });
 
-        test('Then we could click in "Mis productos', () => {
-            const mockhandleLogout = jest.fn();
-            (useUserAuth as jest.Mock).mockReturnValue({
-                handleLoginWithGoogle: jest.fn(),
-                handleLogout: mockhandleLogout,
-            });
-
+        test('Then we could click in "Mis Cuenta', () => {
             render(
                 <Provider store={mockStore}>
                     <MemoryRouter>
@@ -121,6 +92,23 @@ describe('Given Menu component', () => {
             userEvent.click(buttonElement2[0]);
 
             expect(buttonElement2[0]).toHaveAttribute(
+                'class',
+                'nav-button nav-button--active'
+            );
+        });
+        test('Then we should in "Publicar"  check is active class', () => {
+            render(
+                <Provider store={mockStore}>
+                    <MemoryRouter>
+                        <Menu />
+                    </MemoryRouter>
+                </Provider>
+            );
+            const buttonElement = screen.getAllByText('Publicar');
+            expect(buttonElement[0]).toHaveAttribute('class', 'nav-button');
+            userEvent.click(buttonElement[0]);
+
+            expect(buttonElement[0]).toHaveAttribute(
                 'class',
                 'nav-button nav-button--active'
             );
