a051297aa220c63767ed89d2453cbc33ebce5056
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-15 13:35:15 +0100

    :construction: Modify menu component to open Modal

--- src/core/components/menu/menu.tsx
+++ src/core/components/menu/menu.tsx
@@ -1,28 +1,42 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { useUserAuth } from '../../hooks/use.user.auth';
 import { RootState } from '../../store/store';
+import * as ac from '../../reducer/action.creator';
+
 import './menu.scss';
 
 export function Menu() {
     const { handleLoginWithGoogle } = useUserAuth();
     const user = useSelector((state: RootState) => state.user);
 
+    const dispatch = useDispatch();
+
     const handleLoginOnClick = () => {
         handleLoginWithGoogle();
+    };
+
+    const handleOpenModalClick = () => {
+        dispatch(ac.openActionCreatorModal());
     };
 
     const activeClassName = 'nav-button nav-button--active';
     return (
         <nav className="top-menu">
-            <NavLink
-                className={({ isActive }) =>
-                    isActive ? activeClassName : 'nav-button'
-                }
-                to={'/subir-producto'}
-            >
-                Publicar
-            </NavLink>
+            {user.info?.firebaseId ? (
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? activeClassName : 'nav-button'
+                    }
+                    to={'/subir-producto'}
+                >
+                    Publicar
+                </NavLink>
+            ) : (
+                <p className="nav-button" onClick={handleOpenModalClick}>
+                    Publicar
+                </p>
+            )}
 
             {user.info?.firebaseId && (
                 <NavLink
