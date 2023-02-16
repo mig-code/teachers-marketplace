abd90ec28d1caf667e76c0af4b91d2587005fe5c
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-16 12:27:15 +0100

    :bug: fix item load

--- src/features/details.product/components/details.product.tsx
+++ src/features/details.product/components/details.product.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import { useProducts } from '../../../core/hooks/use.products';
@@ -7,7 +7,6 @@ import { useUserFavorites } from '../../../core/hooks/use.user.favorites';
 import { RootState } from '../../../core/store/store';
 import { ProductStructure } from '../../../core/types/products.types';
 import './details.product.scss';
-import * as ac from '../../../core/reducer/action.creator';
 
 import { ButtonFavorite } from '../../../core/components/button.favorite/button.favorite';
 
@@ -23,17 +22,14 @@ export default function DetailsProduct() {
     const { handleAddToFavorites, handleRemoveFromFavorites } =
         useUserFavorites(item);
 
-    const dispatcher = useDispatch();
-
     async function handleClickAddToFavorites() {
         await handleAddToFavorites();
-        dispatcher(ac.loadCurrentActionCreatorProducts(item));
-        handleQueryProduct(firebaseString);
+        await handleQueryProduct(item.firebaseId);
     }
+
     async function handleClickDeleteFromFavorites() {
         await handleRemoveFromFavorites();
-        dispatcher(ac.loadCurrentActionCreatorProducts(item));
-        handleQueryProduct(firebaseString);
+        await handleQueryProduct(item.firebaseId);
     }
 
     useEffect(() => {
@@ -71,8 +67,8 @@ export default function DetailsProduct() {
                         <div className="details__favorites">
                             {item.isLikedBy
                                 ? '¡Le gusta a  ' +
-                                  item.isLikedBy.users.length +
-                                  ' personas!'
+                                item.isLikedBy.users.length +
+                                ' personas!'
                                 : 'A nadie le gusta todavía'}{' '}
                         </div>
                         <ButtonFavorite
