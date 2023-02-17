52c1f92930974288940853c409641ee7db277a0e
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-17 10:43:49 +0100

    :lipstick: minor css improvements

--- src/core/components/footer/footer.tsx
+++ src/core/components/footer/footer.tsx
@@ -5,8 +5,11 @@ export function Footer() {
             <p>
                 {' '}
                 Made with ❤️ by{' '}
-                <a href="https://github.com/mig-code"> Miguel Pgomez </a> | Isdi
-                Coders Final Project
+                <a href="https://github.com/mig-code/teachers-marketplace">
+                    {' '}
+                    Miguel Pgomez{' '}
+                </a>{' '}
+                | Isdi Coders Final Project
             </p>
         </footer>
     );
--- src/core/components/list/list.tsx
+++ src/core/components/list/list.tsx
@@ -1,8 +1,8 @@
 import Item from '../item/item';
 import { ProductStructure } from '../../types/products.types';
+import './list.scss';
 
 export function List({ products }: { products: ProductStructure[] }) {
-    
     return (
         <div className="list">
             {products.map((item) => (
